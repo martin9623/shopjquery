72a72dadbdb6694cf69c7de9aac0c1af1eee42d9
martin9623 <martingomez.9623@gmail.com>
2021-11-28 21:01:23 -0300

    Mobile first, añadido funcion carrito, carrito guarda cantidad en localstorage

--- script/script.js
+++ script/script.js
@@ -1,13 +1,21 @@
 $(() => {
 
+    //mostrar menu mobile
+
+    $('#btn-menu').click(() => {
+        $('.nav__menu').toggleClass('nav__menu-act');
+    });
+
+
     //class
 
     class Producto {
-        constructor(url, marca, descripcion, precio) {
+        constructor(url, marca, descripcion, precio, cantidad) {
             this.url = url;
             this.marca = marca;
             this.descripcion = descripcion;
             this.precio = precio;
+            this.cantidad = cantidad;
         }
     }
 
@@ -77,7 +85,7 @@ $(() => {
 
     //funcion crear productos en el carrito
 
-    function addCart(img, marca, desc, precio) {
+    function addCart(img, marca, desc, precio, cantidad) {
         $('#cart-cont').append(
             `<div id="cart-card" class="cart-card">
                 <div class="cart-card__cont">
@@ -87,6 +95,11 @@ $(() => {
                 <h5 class="cart-card__title">${marca}</h5>
                 <p class="cart-card__desc">${desc}</p>
                 <p class="cart-card__precio">$${precio}</p>
+                <div id="cart-num-cont" class="cart-btn-cont">
+                        <button id="btn-menos" class="btn-menos btn btn-red">-</button>
+                        <p id="cart-cant" class="cart-cant cart-cantidad">${cantidad}</p>
+                        <button id="btn-mas" class="btn-mas btn btn-blue">+</button>
+                    </div>
             </div>
             `
         );
@@ -119,7 +132,7 @@ $(() => {
         }
     });
 
-    //añadir productos al local storage
+    //añadir productos Admin al local storage
 
     $('#formAdmin').submit((e) => {
         e.preventDefault();
@@ -127,8 +140,6 @@ $(() => {
         let datos = new FormData(e.target);
 
         productos.push(new Producto(datos.get('url'), datos.get('marca'), datos.get('descripcion'), datos.get('precio')));
-
-        console.log(productos);
 
         localStorage.setItem('productos', JSON.stringify(productos));
 
@@ -151,7 +162,7 @@ $(() => {
 
     carrito.forEach(producto => {
         if (producto != null) {
-            addCart(producto.url, producto.marca, producto.descripcion, producto.precio);
+            addCart(producto.url, producto.marca, producto.descripcion, producto.precio, producto.cantidad);
         }
     });
 
@@ -166,7 +177,7 @@ $(() => {
         const desc = nodoPadre.querySelector('.card-desc').textContent;
         const precio = nodoPadre.querySelector('.card-precio').textContent;
 
-        carrito.push(new Producto(url, nombre, desc, precio));
+        carrito.push(new Producto(url, nombre, desc, precio, 1));
 
         localStorage.setItem('carrito', JSON.stringify(carrito));
 
@@ -180,8 +191,43 @@ $(() => {
         location.reload();
     });
     $('#btn-clear-cart').click(() => {
-        localStorage.clear();
+        localStorage.removeItem('carrito');
         location.reload();
     });
 
+    //sumar cantidad cart y guardar en localStorage
+
+    $('.cart-btn-cont').click((e) => {
+        const nodoPadre = e.target.parentNode;
+        const cart = nodoPadre.parentNode;
+        const title = cart.querySelector('.cart-card__title').textContent;
+        const desc = cart.querySelector('.cart-card__desc').textContent;
+        const precio = cart.querySelector('.cart-card__precio').textContent.slice(1);
+
+        const btnMas = nodoPadre.querySelector('.btn-mas');
+        const btnMenos = nodoPadre.querySelector('.btn-menos');
+        let cartCant = nodoPadre.querySelector('.cart-cant');
+
+        let carritoMemoria = localStorage.getItem('carrito');
+
+        carritoMemoria = JSON.parse(carritoMemoria);
+
+        if (e.target === btnMas) {
+            let value = Number(cartCant.textContent);
+            cartCant.textContent = value + 1;
+        } else if (e.target === btnMenos) {
+            let value = Number(cartCant.textContent);
+            if (value > 1) {
+                cartCant.textContent = value - 1;
+            }
+        }
+
+        carritoMemoria.forEach(producto => {
+            if (producto.marca == title && producto.descripcion == desc && producto.precio == precio) {
+                producto.cantidad = cartCant.textContent;
+            }
+        });
+
+        localStorage.setItem('carrito', JSON.stringify(carritoMemoria));
+    });
 });
